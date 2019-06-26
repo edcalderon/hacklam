6a1f80d1d16315d0de59e0506af0d1f5903a3e46
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2019-06-25 22:51:28 -0500

    modified: config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,18 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 
-let urlDB
+let urlDB;
 
-if (process.env.NODE_ENV === 'local'){
+if (process.env.NODE_ENV === 'local') {
 	urlDB = 'mongodb://localhost:27017/app-db';
-	process.env.URLDB = urlDB
+	process.env.URLDB = urlDB;
 }
 
 
 const dotenv = require('dotenv');
+
 dotenv.config();
 module.exports = {
-  URLDB: process.env.URLDB,
-  APIKEY: process.env.APIKEY,
-  PORT: process.env.PORT
+	URLDB: process.env.URLDB,
+	APIKEY: process.env.APIKEY,
+	PORT: process.env.PORT,
 };
